bd6e8b321dfb5fd9989df70c30b03ae2c2032ca5
Viktor Poklad <3d.poklad@gmail.com>
2022-02-08 00:08:12 +0200

    cleanup

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -121,58 +121,6 @@ const googleRedirect = async (req, res) => {
   );
 };
 
-// const googleAuth = async (req, res) => {
-//   console.log(baseURL);
-//   const stringifiedParams = queryString.stringify({
-//     client_id: process.env.GOOGLE_CLIENT_ID,
-//     redirect_uri: `${baseURL}/api/users/google-redirect`,
-//     scope: [
-//       'https://www.googleapis.com/auth/userinfo.email',
-//       'https://www.googleapis.com/auth/userinfo.profile',
-//     ].join(' '),
-//     response_type: 'code',
-//     access_type: 'offline',
-//     prompt: 'consent',
-//   });
-//   return res.redirect(
-//     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`,
-//   );
-// };
-
-// const googleRedirect = async (req, res) => {
-//   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-//   const urlObj = new URL(fullUrl);
-//   const urlParams = queryString.parse(urlObj.search);
-
-//   const code = urlParams.code;
-
-//   const tokenData = await axios({
-//     url: `https://oauth2.googleapis.com/token`,
-//     method: 'post',
-//     data: {
-//       client_id: process.env.GOOGLE_CLIENT_ID,
-//       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-//       redirect_uri: `${baseURL}/api/users/google-redirect`,
-//       grant_type: 'authorization_code',
-//       code,
-//     },
-//   });
-
-//   const userData = await axios({
-//     url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-//     method: 'get',
-//     headers: {
-//       Authorization: `Bearer ${tokenData.data.access_token}`,
-//     },
-//   });
-//   console.log(userData);
-
-//   return res.redirect(
-//     `http://localhost:3000/google-redirect?email=${userData.data.email}`,
-//     // ПОМЕНЯТЬ НА ПРОДЕ
-//   );
-// };
-
 const logout = async (req, res, _next) => {
   await authService.setToken(req.user.id, null);
   res
